11a8143eb8459aac5a5c4485df35cb5a64e38a3d
Sonia <sonia@Sonias-MacBook-Pro.local>
2024-08-15 16:44:22 -0700

    404

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+bjmc8h_1o*=zsls963u4pfv4nlxo@4!scr1a9(h51%x*inj70'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
