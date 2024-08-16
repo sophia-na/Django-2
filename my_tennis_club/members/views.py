dc6b8f1d567546e2db324a6d9d5bdf34a13d2123
Sonia <sonia@Sonias-MacBook-Pro.local>
2024-08-15 19:32:18 -0700

    testing

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -40,10 +40,14 @@ def details(request, id):
   context = {
     'mymember': mymember,
   }
-  return HttpResponse(template.render(context, request))
-
   
 def main(request):
   template = loader.get_template('main.html')
   return HttpResponse(template.render())
-    
+
+def testing(request):
+  template = loader.get_template('template.html')
+  context = {
+    'fruits': ['Apple', 'Banana', 'Cherry'],   
+  }
+  return HttpResponse(template.render(context, request))
