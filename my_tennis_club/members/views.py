5bdead7625179049d4d9d0d1f5f774e5704a1833
sophia-na <amitazh@aol.com>
2024-08-19 16:01:39 -0700

    slug

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,14 +1,26 @@
-# # # from django.shortcuts import render
+# # # # from django.shortcuts import render
+# # # # from django.http import HttpResponse
+
+# # # # def members(request):
+# # # #     return HttpResponse("Hello world!")
 # # # from django.http import HttpResponse
+# # # from django.template import loader
 
 # # # def members(request):
-# # #     return HttpResponse("Hello world!")
+# # #   template = loader.get_template('myfirst.html')
+# # #   return HttpResponse(template.render())
+
 # # from django.http import HttpResponse
 # # from django.template import loader
+# # from .models import Member
 
 # # def members(request):
-# #   template = loader.get_template('myfirst.html')
-# #   return HttpResponse(template.render())
+# #   mymembers = Member.objects.all().values()
+# #   template = loader.get_template('all_members.html')
+# #   context = {
+# #     'mymembers': mymembers,
+# #   }
+# #   return HttpResponse(template.render(context, request))
 
 # from django.http import HttpResponse
 # from django.template import loader
@@ -21,7 +33,24 @@
 #     'mymembers': mymembers,
 #   }
 #   return HttpResponse(template.render(context, request))
+  
+# def details(request, id):
+#   mymember = Member.objects.get(id=id)
+#   template = loader.get_template('details.html')
+#   context = {
+#     'mymember': mymember,
+#   }
+  
+# def main(request):
+#   template = loader.get_template('main.html')
+#   return HttpResponse(template.render())
 
+# def testing(request):
+#   template = loader.get_template('template.html')
+#   context = {
+#     'fruits': ['Apple', 'Banana', 'Cherry'],   
+#   }
+#   return HttpResponse(template.render(context, request))
 from django.http import HttpResponse
 from django.template import loader
 from .models import Member
@@ -34,12 +63,13 @@ def members(request):
   }
   return HttpResponse(template.render(context, request))
   
-def details(request, id):
-  mymember = Member.objects.get(id=id)
+def details(request, slug):
+  mymember = Member.objects.get(slug=slug)
   template = loader.get_template('details.html')
   context = {
     'mymember': mymember,
   }
+  return HttpResponse(template.render(context, request))
   
 def main(request):
   template = loader.get_template('main.html')
@@ -50,4 +80,4 @@ def testing(request):
   context = {
     'fruits': ['Apple', 'Banana', 'Cherry'],   
   }
-  return HttpResponse(template.render(context, request))
+  return HttpResponse(template.render(context, request))  
